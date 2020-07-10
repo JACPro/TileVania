ef5e78dbba085ece55178e7046bf6da4e407cb54
JACPro <jamesacpro@gmail.com>
2020-07-10 17:03:03 +0100

    Added run, climb and jump movement and animations

---
+++ Assets/Asset Packs/upm-package-cinemachine-2018 - Copy/upm-package-cinemachine-2018/QAReport.md
@@ -0,0 +1,22 @@
+# Quality Report
+Use this file to outline the test strategy for this package.
+
+## QA Owner: [*Joel Fortin*]
+## UX Owner: [*Joel Fortin*]
+
+Cinemachine 2.2.9 and 2.3.4 Testing Report
+
+- Regression testing on overall functionalities
+- Example package integrity tested
+- Bug fixes validated
+  - BlendList improvements
+  - Collider improvements
+  - FreeLook guildline display fixes
+  - Framing Transposer Center on Activate
+  - Blend Interuption Cut fix
+  - Fogbugz defect validated and closed:
+    - [1150847] Cinemachine brain cut even doesn't trigger when cutting to a camera blend
+    - [1138263] Two sets of frame guidelines are drawn when FreeLook camera is used
+    - [1119028] [Cinemachine] Using RemoveMember() sets the last member of the Target Group to null
+- PostProcessing HDRP Extension Tested (VolumeSettings)
+- General Performance improvement untested but no regression was found
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityStandardAssets.CrossPlatformInput;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    //Config
+    [SerializeField] float runSpeed;
+    [SerializeField] float jumpStrength;
+    [SerializeField] float climbSpeed;
+    
+    //State
+    bool isAlive = true;
+
+    //Cached component references
+    Rigidbody2D rb; 
+    Animator animator;
+    float startingGravity;
+
+    //Methods
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        startingGravity = rb.gravityScale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Run();
+
+        //if player is on ground
+        if (CrossPlatformInputManager.GetButtonDown("Jump")) {
+            if (GetComponent<Collider2D>().IsTouchingLayers(LayerMask.GetMask("Foreground"))) { //if is in contact with foreground tiles
+                Jump();
+            }
+        }
+
+        //if player is touching a ladder
+        if (GetComponent<Collider2D>().IsTouchingLayers(LayerMask.GetMask("Climbing"))) {
+            ClimbLadder();
+        }
+        else
+        {
+            animator.SetBool("Climbing", false);
+            rb.gravityScale = startingGravity;
+        }
+    }
+
+    private void Run()
+    {
+        float controlThrow = CrossPlatformInputManager.GetAxis("Horizontal"); //value between -1 and 1
+        Vector2 playerVelocity = new Vector2(controlThrow * runSpeed, rb.velocity.y);
+        rb.velocity = playerVelocity;
+
+        bool isRunning = controlThrow != 0;
+        FlipPlayer(isRunning);
+        animator.SetBool("Running", isRunning);
+    }
+
+    private void Jump() {        
+        rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y + jumpStrength);
+    }
+
+    private void ClimbLadder() {
+        rb.gravityScale = 0;
+        float controlThrow = CrossPlatformInputManager.GetAxis("Vertical"); //value between -1 and 1
+        Vector2 climbVelocity = new Vector2(rb.velocity.x, controlThrow * climbSpeed);
+
+        Debug.Log(controlThrow);
+
+        rb.velocity = climbVelocity;
+
+        bool playerHasVerticalSpeed = Mathf.Abs(rb.velocity.y) > Mathf.Epsilon;
+        Debug.Log(playerHasVerticalSpeed);
+        animator.SetBool("Climbing", playerHasVerticalSpeed);
+    }
+
+    private void FlipPlayer(bool isRunning) {
+        if (isRunning) {
+            transform.localScale = new Vector2(Mathf.Sign(rb.velocity.x), transform.localScale.y);
+        }
+    }
+}
